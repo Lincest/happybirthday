899df158e49c53ccfc2f30920c868fbb383dc402
Lincest <474617032@qq.com>
2020-08-01 20:28:51 +0800

    init

---
+++ time.js
@@ -0,0 +1,18 @@
+function BirthDay() {
+    window.setTimeout("BirthDay()", 1000);
+        BirthDay = new Date("08/01/2000 00:00:00"); // 修改出生日期
+        today = new Date();
+        timeold = (today.getTime() - BirthDay.getTime());
+        sectimeold = timeold / 1000
+        secondsold = Math.floor(sectimeold);
+        msPerDay = 24 * 60 * 60 * 1000
+        e_daysold = timeold / msPerDay
+        daysold = Math.floor(e_daysold);
+        e_hrsold = (e_daysold - daysold) * 24;
+        hrsold = Math.floor(e_hrsold);
+        e_minsold = (e_hrsold - hrsold) * 60;
+        minsold = Math.floor((e_hrsold - hrsold) * 60);
+        seconds = Math.floor((e_minsold - minsold) * 60);
+        span_dt_dt.innerHTML = daysold + "天" + hrsold + "小时" + minsold + "分" + seconds + "秒";
+    }
+    BirthDay();
